655ad4f68b950bfb786278b11efb30766b5b5223
superman1006 <1402788264@qq.com>
2022-11-19 15:20:54 +0800

    1

--- Vue3/vue_test/src/App.vue
+++ Vue3/vue_test/src/App.vue
@@ -2,7 +2,7 @@
  * @Author: superman1006 1402788264@qq.com
  * @Date: 2022-11-19 11:01:30
  * @LastEditors: superman1006 1402788264@qq.com
- * @LastEditTime: 2022-11-19 12:34:53
+ * @LastEditTime: 2022-11-19 15:14:13
  * @FilePath: \Vue_PROJECT\Vue3\Vue学习\26_单文件组件\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
--- Vue3/vue_test/src/main.js
+++ Vue3/vue_test/src/main.js
@@ -1,3 +1,19 @@
+/*
+ * @Author: superman1006 1402788264@qq.com
+ * @Date: 2022-11-19 11:42:17
+ * @LastEditors: superman1006 1402788264@qq.com
+ * @LastEditTime: 2022-11-19 15:17:41
+ * @FilePath: \Vue_PROJECT\Vue3\vue_test\src\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
+ * @Author: superman1006 1402788264@qq.com
+ * @Date: 2022-11-19 11:42:17
+ * @LastEditors: superman1006 1402788264@qq.com
+ * @LastEditTime: 2022-11-19 15:04:22
+ * @FilePath: \Vue_PROJECT\Vue3\vue_test\src\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // 该文件是整个项目的入口文件
 // 引入Vue
 import { createApp } from 'vue'
