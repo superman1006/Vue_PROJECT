c85681ef96c7792ca5c0c9cc8c1a92c9c86bfd0a
superman1006 <1402788264@qq.com>
2023-04-18 17:41:59 +0800

    1

--- JavaScript/Web大作业/introduction.js
+++ JavaScript/Web大作业/introduction.js
@@ -1,9 +1,5 @@
-var navLinks = document.querySelectorAll(".nav-link");
-
-for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("mouseover", function () {
-    var current = document.querySelector(".nav-item .active");
-    current.classList.remove("active");
-    this.parentNode.classList.add("active");
+$(document).ready(function () {
+  $(".nav-item").hover(function () {
+    $(this).addClass("active-nav-item").siblings().removeClass("active-nav-item");
   });
-}
+});
